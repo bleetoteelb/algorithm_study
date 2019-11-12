73bff160cab18811ae4f88ac477ab376a738be07
bleetoteelb <sh1993park@gmail.com>
2019-11-12 22:39:08 +0900

    update lcs algorithm

---
+++ 02_substring_diff/lcs.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+
+using namespace std;
+
+int substringDiff(string s1, string s2)
+{
+	int len1 = s1.length(), len2 = s2.length();
+	int max = 0, position=0;
+	int count[2][1501]={0,};
+
+
+	for(int i=0;i<len1;i++)	{
+		for(int ii=0;ii<len2;ii++) {
+			if(s1[i] == s2[ii]){
+				if(i==0|ii==0) count[position][ii] = 1;
+				else count[position][ii] = count[position?0:1][ii-1] + 1;
+				
+				if(count[position][ii]>max) max = count[position][ii];
+				position=position?0:1;
+			}
+		}
+	}
+	return max;
+}
+
+int main()
+{
+	clock_t time = clock();
+
+    int t;
+    cin >> t;
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+		string s1, s2;
+		cin >> s1 >> s2;
+
+        int result = substringDiff(s1, s2);
+
+        cout << result << "\n";
+    }
+
+
+	time = clock() - time;
+	printf("It takes %f secods\n",((float)time/CLOCKS_PER_SEC));
+
+    return 0;
+}
+
+
