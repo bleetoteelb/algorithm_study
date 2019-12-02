05e59547cb107c250915bc30dabc04bf2ae74bbd
bleetoteelb <sh1993park@gmail.com>
2019-12-02 17:06:30 +0900

    xor and sum update

--- 04_xor_and_sum/main.cpp
+++ 04_xor_and_sum/main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 int xorAndSum(string a, string b){
 	long long result = 0, cur = 1;
-	int ai[50000], bi[50000];
-	int cnt[50000][2];
+	int ai[500000], bi[500000];
+	int cnt[500000][2];
 	int cnt0 = 0, cnt1 = 0;
 	const int sig = 314159;
 	const int mod = 1000000007;
@@ -20,12 +20,12 @@ int xorAndSum(string a, string b){
 	int sizeb = b.length();
 	for(int i=0;i<sizeb;i++) bi[sizeb-i-1] = b[i] - '0';
 
-	cnt[0][0] = (b[0] == 0);
-	cnt[1][1] = (b[0] == 1);	
+	cnt[0][0] = (bi[0] == 0);
+	cnt[0][1] = (bi[0] == 1);	
 
 	for(int i=1;i<sizeb+sig;i++){
-		cnt[i][0] = cnt[i-1][0] + (b[i] == 0);
-		cnt[i][1] = cnt[i-1][1] + (b[i] == 1);
+		cnt[i][0] = cnt[i-1][0] + (bi[i] == 0);
+		cnt[i][1] = cnt[i-1][1] + (bi[i] == 1);
 	}
 	
 	for(int i=0;i<sizeb+sig;i++){
@@ -39,33 +39,10 @@ int xorAndSum(string a, string b){
 		
 		if(i!=0) cur=(cur*2)%mod;
 		
-		long long tmp = (cur*((a[i]*cnt0)+((a[i]^1)*cnt1)));
+		long long tmp = (cur*((ai[i]*cnt0)+((ai[i]^1)*cnt1)));
 
 		result = (result+tmp)%mod;
 	}	
-/*
-	int multi = 1;
-	for(int i=0;i<size;i++){
-		AtoTen += (a[size-i-1]-'0')*multi;		
-		multi *= 2;
-	}
-	size = b.length();
-
-	multi = 1;
-	for(int i=0;i<size;i++){
-		BtoTen += (b[size-i-1]-'0')*multi;		
-		multi *= 2;
-	}
-    cout << AtoTen << " " << BtoTen << "\n";
-	for(int i=0;i<=314159;i++){
-		if(i!=0) BtoTen *= 2;
-		if(BtoTen>=2147438648) BtoTen -= 2147438648;
-		result += AtoTen ^ BtoTen;
-		if(result>=214738648) result -= 214738648;
-		cout << (AtoTen ^ BtoTen) << " : " << result << endl;
-	}
-	result %= 1000000007;
-*/
 	return result;
 }
 
