b25d6daab9f910395ad614ece67a2c73d35486f0
bleetoteelb <sh1993park@gmail.com>
2019-12-07 18:45:43 +0900

    sam and substrings update

--- 05_sam_and_substrings/main.cpp
+++ 05_sam_and_substrings/main.cpp
@@ -7,24 +7,27 @@ using namespace std;
 
 
 int substrings(string n){
-	long long result = 0;
-	int cnt[6] = {0,};
-	int ai[6] = {0,};
 	const int mod = 1000000007;
+	const int con = 200000;
+	long result = 0;
+	long cnt[con] = {0,};
+	int ai[con] = {0,};
 
 	int size = n.length();
-	for(int i=0;i<size;i++) ai[6-i-1] = n[size-i-1] - '0';
+	for(int i=0;i<size;i++) ai[con-i-1] = n[size-i-1] - '0';
 	
 	cnt[0] = ai[0];
-	for(int i=1;i<6;i++){
-		cnt[i] = cnt[i-1] + ai[i]*(i-(6-size)+1); 
+	for(int i=1;i<con;i++){
+		cnt[i] = cnt[i-1] + ai[i]*(i-(con-size)+1); 
+		cnt[i] %= mod;
 	}
-	
-	int iter = 1;
-	for(int i=0;i<6;i++){
-		result += iter*cnt[6-i-1];
+
+	long iter = 1;
+	for(int i=0;i<size;i++){
+		
+		result += iter*cnt[con-i-1];
 		result %= mod;
-		iter *= 10;
+		iter = (iter*10)%mod;
 	}	
 	return result;
 }
