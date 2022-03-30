9fb14d6156c4a7f000412ca045d837c954552122
bleetoteelb <sh1993park@gmail.com>
2022-03-31 08:39:31 +0900

    Update problem link

--- leetcode/search-a-2d-matrix.py
+++ leetcode/search-a-2d-matrix.py
@@ -2,35 +2,29 @@
 class Solution:
     def searchMatrix(self, matrix, target) -> bool:
         rStart, rEnd, cStart, cEnd = 0, len(matrix)-1, 0, len(matrix[0])-1
-        print(rStart, rEnd, cStart, cEnd)
-        targetRow = (rStart + rEnd)//2
-        print(targetRow)
-        print("------------")
+        targetRow = (rStart + rEnd+1)//2
         while(True):
-            print(rStart,rEnd,targetRow)
-            if matrix[targetRow][0] < target:
-                rEnd = targetRow
-            else:
-                rStart = targetRow
-            targetRow = (rStart + rEnd)//2
-            if rStart == rEnd:
+            if matrix[targetRow][0] > target:
+                rEnd = targetRow - 1
+            elif matrix[targetRow][0] <= target and matrix[targetRow][-1] >= target:
                 break
+            elif targetRow+1 < len(matrix) and matrix[targetRow+1][0] <= target:
+                rStart = targetRow +1
+            else:
+                return "false"
+            targetRow = (rStart + rEnd+1)//2
 
-        print("------------")
-        print("targetRow",targetRow)
-        targetCol = (cStart + cEnd)//2
-        print(targetCol)
-        print("------------")
+        targetCol = (cStart + cEnd+1)//2
         while(True):
-            print(cStart,cEnd,targetCol)
-            if matrix[targetRow][targetCol] < target:
-                cEnd = targetCol
+            if matrix[targetRow][targetCol] == target:
+                return "true"
+            elif matrix[targetRow][targetCol] > target:
+                cEnd = targetCol-1
             else:
-                cStart = targetCol
-            targetCol = (cStart + cEnd)//2
-            if cStart == cEnd:
+                cStart = targetCol+1
+            targetCol = (cStart + cEnd+1)//2
+            if cStart >= cEnd:
                 break
-        print("targetCol",targetCol)
         if matrix[targetRow][targetCol] == target:
             return "true"
         else:
@@ -39,7 +33,12 @@ class Solution:
 
 
 inputMatrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
-inputTarget = 11
 solution = Solution()
-print(solution.searchMatrix(inputMatrix, inputTarget))
+print(solution.searchMatrix(inputMatrix, 13)) # false
+print(solution.searchMatrix(inputMatrix, 4))  # false
+print(solution.searchMatrix(inputMatrix, 50)) # false
+print(solution.searchMatrix(inputMatrix, 80)) # false
+print(solution.searchMatrix(inputMatrix, 3)) # true
+print(solution.searchMatrix(inputMatrix, 20)) # true
+print(solution.searchMatrix(inputMatrix, 23)) # true
         
